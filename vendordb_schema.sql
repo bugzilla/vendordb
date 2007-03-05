b7e3bf1fbbb8f3c4baeef43c16b4ee4c71ac1625
Dave Miller <justdave@bugzilla.org>
2007-03-05 09:38:51 +0000

    Add table schema

---
+++ vendordb_schema.sql
@@ -0,0 +1,43 @@
+-- MySQL dump 10.9
+--
+-- Host: 10.2.70.105    Database: bugzilla_org_vendordb
+-- ------------------------------------------------------
+-- Server version	4.1.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `vendors`
+--
+
+DROP TABLE IF EXISTS `vendors`;
+CREATE TABLE `vendors` (
+  `vID` smallint(6) NOT NULL auto_increment,
+  `name` tinytext character set latin1,
+  `website` tinytext character set latin1,
+  `services` tinytext character set latin1,
+  `location` tinytext character set latin1,
+  `willwork` tinytext character set latin1,
+  `contact` tinytext character set latin1,
+  `comment` text character set latin1,
+  `approved` tinyint(4) NOT NULL default '0',
+  `country` tinytext,
+  `region` tinytext,
+  PRIMARY KEY  (`vID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
